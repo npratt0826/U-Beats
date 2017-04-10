13c55a50e8530f20df151e1ed19c6cabc212e063
npratt0826 <nicholasapratt@gmail.com>
2017-04-10 18:18:58 -0400

    Checkpoint-8-events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,26 @@
+var pointsArray = document.getElementsByClassName("point");
 
-var revealPoint = function(){
-    var animatePoints = document.getElementsByClassName("point");
-    for(var i=0; i < animatePoints.length; i++){
-        animatePoints[i].style.opacity = 1;
-        animatePoints[i].style.transform = "scaleX(1) translateY(0)";
-        animatePoints[1].style.msTransform = "scaleX(1) translateY(0)";
-        animatePoints[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function(points){
+    
+    var revealPoint = function(i){
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     
     }
+    for(var i=0; i < points.length; i++){
+        revealPoint(i);
+    }
+}
+
+window.onload = function(){
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) { animatePoints(pointsArray)}
+        
+    });
 }
 
    
